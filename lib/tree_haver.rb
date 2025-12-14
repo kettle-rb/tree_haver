a89211bff10f4440b96758a8ac9d7d539001b0c8
Peter H. Boling <peter.boling@gmail.com>
2025-12-13 22:19:02 -0700

    🎉 Tada!

---
+++ lib/tree_haver.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tree_haver/version"
+
+module TreeHaver
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tree_haver_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TreeHaver do
+  it "has a version number" do
+    expect(TreeHaver::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
